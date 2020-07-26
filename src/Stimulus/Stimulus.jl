442e8e7418e86196369b489969be790fab0af018
Yizhan Miao <yizhanmiao@pm.me>
2020-07-26 15:38:17 -0700

    equal_division

--- src/Stimulus/Stimulus.jl
+++ src/Stimulus/Stimulus.jl
@@ -2,3 +2,24 @@ abstract type StimulusEnsemble end
 
 include("SparseNoise.jl")
 include("GaussianNoise.jl")
+
+function equal_division(ids::Vector{T}, size::Integer; total=512) where {T <: Integer}
+    _train_idx = zeros(Int, length(ids) - total * size)
+    _test_idx = zeros(Int, total * size)
+
+    n_train = 0
+    n_test = 0
+
+    for idx in 1:total
+        _candidates = findall(ids .== idx-1)
+        rand_queue = randperm(length(_candidates))
+
+        _train_idx[n_train+1:n_train+length(rand_queue)-size] = _candidates[rand_queue[size+1:end]]
+        _test_idx[n_test+1:n_test+size] = _candidates[rand_queue[1:size]]
+
+        n_train += length(rand_queue) - size
+        n_test += size
+
+    end
+
+    return (_train_idx, _test_idx)
