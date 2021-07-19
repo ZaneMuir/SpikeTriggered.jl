d2ec0281a27cf2bf933dd9ecbdef3a3682a71117
Yizhan Miao <yizhanmiao@pm.me>
2021-07-19 10:51:49 -0700

    process_first_of_paired_markers

--- src/Stimulus/Stimulus.jl
+++ src/Stimulus/Stimulus.jl
@@ -3,4 +3,10 @@ module Stimulus
 import Base
 
 include("sparse_noise.jl")
+
+function process_first_of_paired_markers(m::Vector{T}) where {T}
+    @assert length(m) % 2 == 0 "markers have to be paired!"
+    m[1:2:end-1]
+end
+
 end
