47b56563d20d63b60ccfac209c658e3836b79d8e
Yizhan Miao <yizhanmiao@pm.me>
2021-10-11 17:05:14 -0700

    chore: move gsl_gaussian_filter to waveform module

--- src/PSTH/filter.jl
+++ src/PSTH/filter.jl
@@ -7,31 +7,3 @@ function spk_filter(spk::Vector{T}, proj, kernel::Function) where {T <: Real}
     end
     return _psth
 end
-
-@doc raw"""
-    gsl_gaussian_filter(arr::Vector{Float64}; K::Integer, α::Real=1)
-
-The GSL gaussian kernel is defined by:
-```math
-G(k) = \exp\left(- \frac{k^2}{2 \sigma^2}\right)
-```
-
-for ``-(K-1)/2 \leq k \leq (K-1)/2``, and ``K`` is the size of the kernel.
-The parameter ``\alpha`` specifies the number of standard deviations ``\sigma``
-desired in the kernel.
-```math
-\sigma = \frac{K-1}{2 \alpha}
-```
-"""
-function gsl_gaussian_filter(arr::Vector{Float64}; K::Integer, α::Real=1)
-    _output = zeros(length(arr))
-
-    _x = GSL.C.gsl_vector(length(arr), 1, pointer(arr), C_NULL, 0)
-    _y = GSL.C.gsl_vector(length(_output), 1, pointer(_output), C_NULL, 0)
-
-    _workspace = GSL.filter_gaussian_alloc(K)
-    GSL.filter_gaussian(GSL.GSL_FILTER_END_PADZERO, α, 0, _x, _y, _workspace)
-    GSL.filter_gaussian_free(_workspace)
-
-    return _output
-end
