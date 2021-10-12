29dc58269809cfd85c1388f6139e218216277eb2
Yizhan Miao <yizhanmiao@pm.me>
2021-10-11 22:08:48 -0700

    new: trace_event_related

--- src/Waveforms/Waveforms.jl
+++ src/Waveforms/Waveforms.jl
@@ -5,8 +5,14 @@ include("events.jl")
 include("filter.jl")
 
 #TODO: ERP function
-# function trace_event_related(trace::Vector{T}, markers::Vector{T}; head=0.5, duration=1, tail=0.5) where {T <: Real}
-
-# end
+function trace_event_related(trace::Vector{T}, markers::Vector{T}; head=0.5, duration=1, tail=0.5, freq=10_000) where {T} # -> Vector{Vector{T}}
+    output = Vector{Vector{T}}()
+    _tidx = range(1/freq/2, step=1/freq, length=length(trace))
+    for item in markers
+        _roi = (item - head) .< _tidx .<= (item + duration + tail)
+        push!(output, trace[_roi])
+    end
+    output
+end
 
 end
