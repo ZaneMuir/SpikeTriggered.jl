3ecde5367a01765b0f218cf6649fdf9d72be3bb6
Yizhan Miao <yizhanmiao@pm.me>
2021-10-11 17:30:27 -0700

    update

--- src/Waveforms/Waveforms.jl
+++ src/Waveforms/Waveforms.jl
@@ -1,5 +1,12 @@
 module Waveforms
 import GSL
 
+include("events.jl")
 include("filter.jl")
+
+#TODO: ERP function
+# function trace_event_related(trace::Vector{T}, markers::Vector{T}; head=0.5, duration=1, tail=0.5) where {T <: Real}
+
+# end
+
 end
