4f811091f56789244b9e2f46f9349ed1c16b6d7b
Yizhan Miao <yizhanmiao@pm.me>
2021-10-11 18:10:44 -0700

    hot fix: wrong file name

--- src/SpikeTriggered.jl
+++ src/SpikeTriggered.jl
@@ -2,7 +2,7 @@ module SpikeTriggered
 
 include("PSTH/PSTH.jl") # ==> binned spikes
 include("Stimulus/Stimulus.jl") # ==> stimulus matrix
-include("OnlineSpikes/OnlineSpikes.jl") # ==> simple event detection and removal
+include("Waveforms/Waveforms.jl") # ==> simple event detection and removal
 include("Stats/Stats.jl") # ==> spike related statistics: sta, stc, burst, power spectrum, etc.
 
 end # module
