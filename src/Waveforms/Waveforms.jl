4659c72d47f5f2c7df4ec130089e9b622040439c
Yizhan Miao <yizhanmiao@pm.me>
2021-11-15 10:17:22 -0800

    update: erp optimization

--- src/Waveforms/Waveforms.jl
+++ src/Waveforms/Waveforms.jl
@@ -7,12 +7,18 @@ include("filter.jl")
 
 #TODO: ERP function
 function trace_event_related(trace::Vector{T}, markers::Vector{T}; head=0.5, duration=1, tail=0.5, freq=10_000) where {T} # -> Vector{Vector{T}}
-    output = Vector{Vector{T}}()
-    _tidx = range(1/freq/2, step=1/freq, length=length(trace))
-    for item in markers
-        _roi = (item - head) .< _tidx .<= (item + duration + tail)
-        push!(output, trace[_roi])
+    output = Vector{Vector{T}}(undef, length(markers))
+    
+    _N = length(trace)
+    _marker_idx = round.(Int64, markers .* freq) .+ 1
+    _start_offset = round(Int64, head * freq) - 1
+    _end_offset = round(Int64, (duration+tail) * freq) - 1
+    
+    for (idx, item) in enumerate(_marker_idx)
+        _roi = max(1, item-_start_offset):min(_N, item+_end_offset)
+        @inbounds output[idx] = trace[_roi]
     end
+
     output
 end
 
