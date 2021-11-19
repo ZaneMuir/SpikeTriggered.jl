4a1bd54b60a613820b760e6959250ae36e0a2832
Yizhan Miao <yizhanmiao@pm.me>
2021-11-19 14:07:27 -0800

    update: bias could be :on, :off, :diff

--- src/Stimulus/Stimulus.jl
+++ src/Stimulus/Stimulus.jl
@@ -9,10 +9,33 @@ function process_first_of_paired_markers(m::Vector{T}) where {T}
     m[1:2:end-1]
 end
 
-#TODO: doc
+@doc raw"""
+    apply_video_nonlinearity(video, bias=0) -> video
+
+For sparse noise stimuli, to retrieve nonlinear map (ON map and OFF map)
+by ignore bright/dark spots.
+
+# Arguments
+- video: the original video matrix
+- bias: could be either values or symbols
+    - ON: `:on` or `-1`
+    - OFF: `:off` or `1`
+    - DIFF: `:diff` or `0`
+"""
 function apply_video_nonlinearity(video, bias=0)
+    _bias = if isa(bias, Symbol)
+        if bias == :on
+            1
+        elseif bias == :off
+            -1
+        else # e.g. :diff
+            0
+        end
+    else
+        bias
+    end
     _video = copy(video)
-    _video[_video .== bias] .= 0
+    _video[_video .== _bias] .= 0
     _video
 end
 
