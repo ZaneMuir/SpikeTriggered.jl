9a3fd20817ab08c9174656f2ce674601bbc89719
Yizhan Miao <yizhanmiao@pm.me>
2021-12-15 21:36:05 -0800

    move process_first_of_paired_markers to markers.jl

--- src/Stimulus/Stimulus.jl
+++ src/Stimulus/Stimulus.jl
@@ -4,11 +4,6 @@ import Base
 
 include("sparse_noise.jl")
 
-function process_first_of_paired_markers(m::Vector{T}) where {T}
-    @assert length(m) % 2 == 0 "markers have to be paired!"
-    m[1:2:end-1]
-end
-
 @doc raw"""
     apply_video_nonlinearity(video, bias=0) -> video
 
