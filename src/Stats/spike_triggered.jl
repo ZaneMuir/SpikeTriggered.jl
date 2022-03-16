755ec40d9a6190f062e606e8f6931e27f935e97a
Yizhan Miao <yizhanmiao@pm.me>
2022-03-15 22:14:35 -0700

    new: spike triggered covariance

--- src/Stats/spike_triggered.jl
+++ src/Stats/spike_triggered.jl
@@ -29,3 +29,33 @@ function spike_triggered_average(X::AbstractMatrix{T}, y::AbstractArray{T}; n=10
         output[:]
     end
 end
+
+@doc raw"""
+    spike_triggered_covariance(X::AbstractMatrix{T}, y::AbstractArray{T}; n::Integer=10, verbose=true) where {T, U} -> Matrix{T}
+"""
+function spike_triggered_covariance(X::AbstractMatrix{T}, y::AbstractArray{T}; n::Integer=10, verbose=true) where {T}
+    _Nx = size(X, 1)
+    
+    _sta = spike_triggered_average(X, y; n, norm=false)
+    _sta_c = _sta ./ sum(abs2, _sta)
+    
+    _stc = zeros(T, length(_sta), length(_sta))
+    
+    _y = mean(y, dims=2)[:]
+    
+    pbar = PM.Progress(length(_y))
+    
+    for idx in 1:length(_y)
+        _ind = (((idx):(-1):(idx-n+1)) .- 1) .% _Nx .+ 1
+        _ind[_ind .< 1] .= _ind[_ind .< 1] .+ _Nx
+        
+        _s = X[_ind, :][:]
+        _s = _s .- _s' * _sta .* _sta_c
+
+        _stc .+= (_s * _s') .* y[idx]
+        
+        verbose && PM.next!(pbar)
+    end
+    _stc ./= sum(_y) - 1
+    _stc
+end
