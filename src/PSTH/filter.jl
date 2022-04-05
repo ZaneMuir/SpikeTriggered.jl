5874f9d8d6607e1a11fa97a355b1e47666f074e6
Yizhan Miao <yizhanmiao@pm.me>
2022-04-04 21:55:26 -0700

    update: rename `spk_filter` to `spike_filter` and deprecate the older one.

--- src/PSTH/filter.jl
+++ src/PSTH/filter.jl
@@ -1,6 +1,15 @@
 k_gaussian(σ::Real=1) = (x) -> exp(x^2 / (- 2 * σ ^ 2)) / (σ * sqrt(2 * π))
+@doc raw"""
+    spike_filter(spk::Vector{T}, proj, kernel::Function; norm::Bool=true) where {T <: Real} -> Vector{T}
 
-function spk_filter(spk::Vector{T}, proj, kernel::Function; norm::Bool=true) where {T <: Real}
+generating smoothed curve from spike trains. equivalent to the smoothed PSTH.
+
+## Arguments:
+- spk: spike train vector
+- proj: range or vector of timestamps interested
+- kernel: function of the smoothing kernel, in format of `T -> T`
+"""
+function spike_filter(spk::Vector{T}, proj, kernel::Function; norm::Bool=true) where {T <: Real}
     _psth = zeros(T, length(proj))
     for idx in 1:length(proj)
         @inbounds @fastmath _psth[idx] = sum(kernel, spk .- proj[idx])
@@ -11,6 +20,8 @@ function spk_filter(spk::Vector{T}, proj, kernel::Function; norm::Bool=true) whe
         _psth
     end
 end
+
+@deprecate spk_filter(spk, proj, kernel; norm) spike_filter(spk, proj, kernel; norm)
 
 @doc raw"""
     spike_gaussian_filter(spk::Vector{T}, proj; σ=0.03) where {T<:Real} -> Vector{T}
@@ -28,5 +39,5 @@ Gaussian smoothing for a given spike train.
 - `Vector{T}`: smoothed psth with the same length as `proj`.
 """
 function spike_gaussian_filter(spk::Vector{T}, proj; σ=0.03) where {T <: Real}
-    spk_filter(spk, proj, k_gaussian(σ))
+    spike_filter(spk, proj, k_gaussian(σ))
 end
