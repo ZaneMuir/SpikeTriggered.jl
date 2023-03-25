7058ef3cc0a878595e3eb557ceaa685d31da5cf6
Yizhan Miao <yizhanmiao@pm.me>
2023-03-25 14:09:51 -0700

    fix: add post burst silence checking

--- src/Stats/burst.jl
+++ src/Stats/burst.jl
@@ -21,8 +21,9 @@ References:
 ## Returns
 - `burst_list::Vector{Vector{T}}`: groups of burst spikes
 """
+#TODO: detect_burst(spk::Vector{T}; t_silence_pre=0.07, t_silence_post=0.07, t_isi=0.03, nofs::Union{Nothing, Tuple{Integer, Real}}=nothing) where {T <: Real}
 function detect_burst(spk::Vector{T}; t_silence=0.07, t_isi=0.03, nofs::Union{Nothing, Tuple{Integer, Real}}=nothing) where {T <: Real}
-    
+
     # initialization
     burst_list = Vector{Vector{T}}()
     current_burst = Vector{T}()
@@ -31,18 +32,21 @@ function detect_burst(spk::Vector{T}; t_silence=0.07, t_isi=0.03, nofs::Union{No
     # given the spike numbers are usually large, it is okay to skip the first and the last bursts, if any.
     for idx in 2:(length(spk)-1)
         if flag_in_burst
-            
+
             # continue
             if spk[idx+1] - spk[idx] <= t_isi
                 push!(current_burst, spk[idx])
-            
+
             # the last one
             else
                 push!(current_burst, spk[idx])
                 flag_in_burst = false
-                push!(burst_list, current_burst)
+                # check the post silence condition
+                if spk[idx+1] - spk[idx] >= t_silence
+                    push!(burst_list, current_burst)
+                end
             end
-            
+
         # seek for potential burst start
         else
             if spk[idx] - spk[idx-1] >= t_silence && spk[idx+1] - spk[idx] <= t_isi
@@ -51,7 +55,7 @@ function detect_burst(spk::Vector{T}; t_silence=0.07, t_isi=0.03, nofs::Union{No
             end
         end
     end
-    
+
     if !isnothing(nofs)
         output = Vector{Vector{T}}()
         while !isempty(burst_list)
@@ -92,13 +96,13 @@ function interp_burst(burst_iti::Vector{T}; n=13, interp_t=nothing) where {T <: 
     accel_obj = GSL.interp_accel_alloc()
 
     GSL.interp_init(interp_obj, xa, iti_list, N)
-    
+
     for (idx, i) in enumerate(range(0, stop=1, length=n))
         output[idx] = GSL.interp_eval(interp_obj, xa, iti_list, Cdouble(i), accel_obj)
     end
 
     GSL.interp_accel_free(accel_obj)
     GSL.interp_free(interp_obj)
-    
+
     output
 end
