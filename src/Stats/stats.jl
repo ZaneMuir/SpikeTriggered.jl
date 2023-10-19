85eef110ad7b209e38d8e3529e22f92e7d1d13e5
Yizhan Miao <yizhanmiao@pm.me>
2023-10-19 14:28:01 -0700

    normcdf and norminv

--- src/Stats/stats.jl
+++ src/Stats/stats.jl
@@ -17,3 +17,9 @@ Check out more on [wikipedia](https://en.wikipedia.org/wiki/Index_of_dispersion)
 function dispersion_index(psth; corrected=true, kwargs...)
     var(psth; corrected, kwargs...) ./ mean(psth; kwargs...)
 end
+
+# gaussian distribution cumulative density, single sided
+normcdf(x::T) where {T <: Real} = erfc(- x / sqrt(T(2))) / 2
+
+# inverse of normcdf
+norminv(x::T) where {T <: Real} = erfcinv(2 * x) * (- sqrt(T(2)))
